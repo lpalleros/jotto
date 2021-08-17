5333b8fbe068b31f7d61b8da65f729678f320743
lucaspalleros <lucaspalleros@gmail.com>
2021-08-17 20:19:40 -0300

    last changes

---
+++ src/Congrats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
+ */
+function Congrats({success}) {
+    return (
+        <div />
+    )
+}
+
+export default Congrats
---
+++ src/Congrats.test.js
@@ -0,0 +1,9 @@
+import Enzyme, { shallow } from 'enzyme';
+
+import Congrats from './Congrats';
+
+describe('basic render', () => {
+    test('renders without error', () => {
+
+    });
+});
